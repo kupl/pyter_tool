f071280f3a92ef01ecfb3ae4fbf27ef1c49d6f32
marinelay <marinelay7@gmail.com>
2022-07-26 16:15:54 +0900

    Update execute.py

--- my_tool/execute.py
+++ my_tool/execute.py
@@ -21,7 +21,7 @@ class Execute() :
         check_project = project.split('-')
 
         if len(check_project) >= 3 : 
-            if ('sanic' not in check_project[0]) and ('pandas' not in check_project[0]):
+            if 'scikit' in check_project[0] :
                 project = project[:project.rfind('-')]
 
 
@@ -75,7 +75,7 @@ class Execute() :
         project = self.project
         check_project = project.split('-')
         if len(check_project) >= 3 : 
-            if ('sanic' not in check_project[0]) and ('pandas' not in check_project[0]):
+            if 'scikit' in check_project[0] :
                 project = project[:project.rfind('-')]
 
         python_dir = self.pyenv_dir + '/versions/' + project + "/bin/python"
